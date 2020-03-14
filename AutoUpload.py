25677cbca635a2967a44faa0d2a21fed8bcdd7e4
Bucur David <46384319+bucurdavid@users.noreply.github.com>
2020-03-14 11:14:10 +0200

    Fixed some bugs

--- AutoUpload.py
+++ AutoUpload.py
@@ -3,15 +3,15 @@ import time
 from selenium import webdriver
 import pickle
 
-lab=input('What lab do you want to upload (ex: 1,2,3) :')
+lab=input('In wich lab do you want to upload (ex: 1,2,3) :')
 print('**************************************************************************')
-row=input('\nPlease count\nHow many these "[add new file]" are up to your lab(inclusive): ')
+row=input('\n_____________________Please count______________________\nHow many these "[add new file]" are up to your lab(inclusive): ')
 if(int(lab)<10):
     labName='lab0'+lab
 else:
-    labName='lab'+lab  
+    labName='lab'+lab    
 
-print(labName)
+print('We are selecting the ',labName,' problems!')
 
 # function that takes the login information from the txt file 
 def information():
@@ -25,45 +25,54 @@ def information():
     myfile.close()
     return username[1],password[1],folderpath[1]
 
-#login varialbes and folder path
 username,password,folderPath=information()
 
-#Connecting to the website 
-#moving the os path where the txt files are located
-os.chdir(folderPath)
-fileName=os.listdir()
-driver=webdriver.Chrome('chromedriver')
-driver.get('https://helios.utcluj.ro/learn2code/login.php?SID')
-try:
+
+def login(username,password):
     usernameInput=driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td/table/tbody/tr[2]/td/form/div/table/tbody/tr[1]/td[2]/input')
     usernameInput.send_keys(username)
     passwordInput=driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td/table/tbody/tr[2]/td/form/div/table/tbody/tr[2]/td[2]/input')
     passwordInput.send_keys(password)
     loginBtn=driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td/table/tbody/tr[2]/td/form/div/table/tbody/tr[4]/td/button')
     loginBtn.click()
-except:
-    print("An error occured!, Please try again")
 
-#after login selecting the lab you want 
-try:
-    workarea=driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td[1]/div/table/tbody/tr[3]/td')
-    workarea.click()
-    print('The ',labName,' is selected')
-        
-    time.sleep(3)
-    extension='.txt'
+if __name__ == "__main__":
+    #login varialbes and folder path
+    #Connecting to the website 
+    driver=webdriver.Chrome('chromedriver')
+    driver.get('https://helios.utcluj.ro/learn2code/login.php?SID')
     try:
-            for elements in fileName:
-                if(labName in elements and extension in elements):
-                    add_link=driver.find_elements_by_class_name('href2')
-                    add_link[int(row)-1].click()
-                    link=folderPath+'\\'+elements
-                    uploadBtn=driver.find_element_by_class_name('input1').send_keys(link)
-                    save=driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td/table/tbody/tr[2]/td/form/div/table/tbody/tr[7]/td/button')
-                    save.click()
-                    print(elements,' was uploaded!')              
+        print("Loging in....")
+        login(username,password)
+        print("\n You are logged in!")
+        answer=input('\nDo you want to continue?[y/n]')
+        if(answer=='y'):
+            #after login selecting the lab you want 
+            try:
+                workarea=driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td[1]/div/table/tbody/tr[3]/td')
+                workarea.click()
+                time.sleep(3)
+                #add_link[int(lab)].click()
+                print('The ',labName,' is selected')
+                    #moving the os path where the txt files are located
+                extensions='.txt'
+                os.chdir(folderPath)
+                fileName=os.listdir()
+                try:
+                        for elements in fileName:
+                            if(labName in elements and extensions in elements):
+                                add_link=driver.find_elements_by_class_name('href2')
+                                add_link[int(row)-1].click()
+                                link=folderPath+'\\'+elements
+                                uploadBtn=driver.find_element_by_class_name('input1').send_keys(link)
+                                save=driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td[2]/table/tbody/tr[2]/td/table/tbody/tr[2]/td/form/div/table/tbody/tr[7]/td/button')
+                                save.click()
+                                print(elements,' was uploaded!')                  
+                except:
+                    print("Error! Run again!")   
+            except:
+                print("Error!Maybe the lab dosn't exist!")
+        else:
+            exit
     except:
-        print("Error! Run again!")   
-except:
-    print("Error!")
-exit
+        print("Error!")                
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # autoupload
 AutoUpload program for UTCN helios webiste
 
-1. You need to install python\
-2.in the "info" file you need to set a path where all your lab txt files are saved\
-3.The txt files need to contain the word "lab" and wich one. ex: lab01-lab13\
-4.In the "info" file you need to add your username and passowrd\
+1. You need to install python 
+2.in the "info" file you need to set a path where all your lab txt files are saved
+3.The txt files need to contain the word "lab" and wich one. ex: lab01-lab13
+4.In the "info" file you need to add your username and passowrd
 
 
 Link python:
